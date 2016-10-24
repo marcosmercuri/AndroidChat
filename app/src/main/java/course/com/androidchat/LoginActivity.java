55d18ed0196577d5ffcc0fe62be8ed60893c13aa
Marcos Mercuri <marcosmercuri@gmail.com>
2016-10-24 23:58:04 +0200

    Add layout objects in activity

--- app/src/main/java/course/com/androidchat/LoginActivity.java
+++ app/src/main/java/course/com/androidchat/LoginActivity.java
@@ -2,12 +2,43 @@ package course.com.androidchat;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
+    @Bind(R.id.editTxtEmail)
+    EditText inputEmail;
+    @Bind(R.id.editTxtPassword)
+    EditText inputPassword;
+    @Bind(R.id.btnSignIn)
+    Button btnSingIn;
+    @Bind(R.id.btnSignUp)
+    Button btnSignUp;
+    @Bind(R.id.progressBar)
+    ProgressBar progressBar;
+    @Bind(R.id.layoutMainContainer)
+    RelativeLayout container;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnSignIn)
+    public void handleSignIn() {
+
+    }
+
+    @OnClick(R.id.btnSignUp)
+    public void handleSignUp() {
+
     }
 }
