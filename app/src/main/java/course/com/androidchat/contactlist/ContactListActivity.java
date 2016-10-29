1e8b49f7f805b7fd1c40ff8432bf3e162cbe32ef
Marcos Mercuri <marcosmercuri@gmail.com>
2016-10-29 12:16:28 +0200

    Feature: Add empty contactList activity

---
+++ app/src/main/java/course/com/androidchat/contactlist/ContactListActivity.java
@@ -0,0 +1,15 @@
+package course.com.androidchat.contactlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import course.com.androidchat.R;
+
+public class ContactListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_list);
+    }
+}
