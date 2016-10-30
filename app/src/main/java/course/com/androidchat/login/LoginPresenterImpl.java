b2221ac78348eedb064ea7aa4867110b3359fe41
Marcos Mercuri <marcosmercuri@gmail.com>
2016-10-30 17:24:42 +0100

    Feature: Add implementation for LoginPresenter

---
+++ app/src/main/java/course/com/androidchat/login/LoginPresenterImpl.java
@@ -0,0 +1,73 @@
+package course.com.androidchat.login;
+
+public class LoginPresenterImpl implements LoginPresenter {
+    LoginView loginView;
+    LoginInteractor loginInteractor;
+
+    public LoginPresenterImpl(LoginView loginView) {
+        this.loginView = loginView;
+    }
+
+    @Override
+    public void onDestroy() {
+        loginView = null;
+    }
+
+    @Override
+    public void checkForAuthenticatedUser() {
+        disableInputsInView();
+        loginInteractor.checkSession();
+    }
+
+    private void disableInputsInView() {
+        if (loginView != null) {
+            loginView.disableInputs();
+            loginView.showProgress();
+        }
+    }
+
+    private void enableInputsInView() {
+        if (loginView != null) {
+            loginView.enableInputs();
+            loginView.hideProgress();
+        }
+    }
+
+    @Override
+    public void validateLogin(String email, String password) {
+        disableInputsInView();
+        loginInteractor.doSignIn(email, password);
+    }
+
+    @Override
+    public void registerNewUser(String email, String password) {
+        disableInputsInView();
+        loginInteractor.doSignUp(email, password);
+    }
+
+    private void onSignInSuccess() {
+        if (loginView!=null) {
+            loginView.navigateToMainScreen();
+        }
+    }
+
+    private void onSignUpSuccess() {
+        if (loginView!=null) {
+            loginView.newUserSuccess();
+        }
+    }
+
+    private void onSignInError(String errorMsg) {
+        if (loginView!=null) {
+            enableInputsInView();
+            loginView.showLoginError(errorMsg);
+        }
+    }
+
+    private void onSignUpError(String errorMsg) {
+        if (loginView!=null) {
+            enableInputsInView();
+            loginView.newUserError(errorMsg);
+        }
+    }
+}
