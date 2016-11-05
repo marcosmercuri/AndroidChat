f085be8a085054bdc8850b45e2648fb0f3853f69
Marcos Mercuri <marcosmercuri@gmail.com>
2016-11-05 12:49:57 +0100

    Feature: Add new version of firebase and the signIn/signUp functionality

--- app/src/main/java/course/com/androidchat/helper/FirebaseHelper.java
+++ app/src/main/java/course/com/androidchat/helper/FirebaseHelper.java
@@ -1,52 +1,55 @@
 package course.com.androidchat.helper;
 
-import com.firebase.client.AuthData;
-import com.firebase.client.DataSnapshot;
-import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
-import com.firebase.client.ValueEventListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import course.com.androidchat.entities.User;
+
 public class FirebaseHelper {
-    private final static String FIREBASE_URL = "https://console.firebase.google.com/project/android-chat-4b149";
     private final static String USERS_PATH = "users";
     private final static String CHATS_PATH = "chats";
     private final static String CONTACTS_PATH = "contacts";
     private final static String SEPARATOR = "___";
-    private Firebase dataReference;
+    private DatabaseReference dataReference;
 
     private static class SingletonHolder {
         private static final FirebaseHelper INSTANCE = new FirebaseHelper();
     }
 
     private FirebaseHelper() {
-        this.dataReference = new Firebase(FIREBASE_URL);
+        this.dataReference = FirebaseDatabase.getInstance().getReference();
     }
 
     public static FirebaseHelper getInstance() {
         return SingletonHolder.INSTANCE;
     }
 
-    public Firebase getDataReference() {
+    public DatabaseReference getDataReference() {
         return dataReference;
     }
 
     public String getAuthUserEmail() {
-        AuthData authData = dataReference.getAuth();
-        if (authData!=null) {
-            return authData.getProviderData().get("email").toString();
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (firebaseUser!=null) {
+            return firebaseUser.getEmail();
         }
         return null;
     }
 
-    public Firebase getAuthUserReference() {
+    public DatabaseReference getAuthUserReference() {
         return getUserReference(getAuthUserEmail());
     }
 
-    public Firebase getUserReference(String email) {
-        Firebase userReference = null;
+    public DatabaseReference getUserReference(String email) {
+        DatabaseReference userReference = null;
         if (isNotBlank(email)) {
             String sanitizeEmail = sanitizeString(email);
             userReference = dataReference.getRoot().child(USERS_PATH).child(sanitizeEmail);
@@ -65,20 +68,20 @@ public class FirebaseHelper {
         return "".equals(email);
     }
 
-    public Firebase getContactsReference(String email) {
+    public DatabaseReference getContactsReference(String email) {
         return getUserReference(email).child(CONTACTS_PATH);
     }
 
-    public Firebase getAuthUserContactsReference() {
+    public DatabaseReference getAuthUserContactsReference() {
         return getContactsReference(getAuthUserEmail());
     }
 
-    public Firebase getOneContactReference(String mainEmail, String childEmail) {
+    public DatabaseReference getOneContactReference(String mainEmail, String childEmail) {
         String sanitizeChildEmail = sanitizeString(childEmail);
         return getUserReference(mainEmail).child(CONTACTS_PATH).child(sanitizeChildEmail);
     }
 
-    public Firebase getAuthUserChatsReference(String receiver) {
+    public DatabaseReference getAuthUserChatsReference(String receiver) {
         String keySender = sanitizeString(getAuthUserEmail());
         String keyReceiver = sanitizeString(receiver);
         String keyChat = getKeyChat(keyReceiver, keySender);
@@ -96,7 +99,7 @@ public class FirebaseHelper {
     }
 
     public void changeUserConnectionStatus(Boolean isOnline) {
-        Firebase userReference = getAuthUserReference();
+        DatabaseReference userReference = getAuthUserReference();
         if (userReference != null) {
             Map<String, Object> updates = new HashMap<>(1);
             updates.put("online", isOnline);
@@ -110,7 +113,7 @@ public class FirebaseHelper {
     }
 
     public void notifyContactsOfSignOff() {
-        notifyContactsOfConnectionChange(false, true);
+        notifyContactsOfConnectionChange(User.OFFLINE, true);
     }
 
     private void notifyContactsOfConnectionChange(final Boolean isOnline, final Boolean signOff) {
@@ -121,16 +124,16 @@ public class FirebaseHelper {
             public void onDataChange(DataSnapshot dataSnapshot) {
                 for (DataSnapshot child : dataSnapshot.getChildren()) {
                     String childEmail = child.getKey();
-                    Firebase reference = getOneContactReference(childEmail, authUserEmail);
+                    DatabaseReference reference = getOneContactReference(childEmail, authUserEmail);
                     reference.setValue(isOnline);
                 }
                 if (signOff) {
-                    dataReference.unauth();
+                    FirebaseAuth.getInstance().signOut();
                 }
             }
 
             @Override
-            public void onCancelled(FirebaseError firebaseError) { }
+            public void onCancelled(DatabaseError databaseError) { }
         });
     }
 }
