1776569949f9f7bc858361538e8ef6bab2546b64
Marcos Mercuri <marcosmercuri@gmail.com>
2016-11-09 11:14:42 +0100

    Feature: Add adapter for the recycler view

--- app/src/main/java/course/com/androidchat/contactlist/ui/adapters/ContactListAdapter.java
+++ app/src/main/java/course/com/androidchat/contactlist/ui/adapters/ContactListAdapter.java
@@ -1,4 +1,97 @@
 package course.com.androidchat.contactlist.ui.adapters;
 
-public class ContactListAdapter {
+import android.graphics.Color;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import course.com.androidchat.R;
+import course.com.androidchat.entities.User;
+import course.com.androidchat.lib.ImageLoader;
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class ContactListAdapter extends RecyclerView.Adapter<ContactListAdapter.ViewHolder> {
+
+    private List<User> contactList;
+    private ImageLoader imageLoader;
+    private OnItemClickListener onItemClickListener;
+
+    public ContactListAdapter(List<User> contactList, ImageLoader imageLoader, OnItemClickListener onItemClickListener) {
+        this.contactList = contactList;
+        this.imageLoader = imageLoader;
+        this.onItemClickListener = onItemClickListener;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.content_contact, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        User user = contactList.get(position);
+        holder.setClickListener(user, onItemClickListener);
+        holder.displayUserInfo(user);
+        imageLoader.load(holder.imgAvatar, "");
+    }
+
+    @Override
+    public int getItemCount() {
+        return contactList.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        @Bind(R.id.imgAvatar)
+        CircleImageView imgAvatar;
+        @Bind(R.id.txtUser)
+        TextView txtUser;
+        @Bind(R.id.txtStatus)
+        TextView txtStatus;
+        private View view;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            this.view = itemView;
+        }
+
+        private void setClickListener(final User user, final OnItemClickListener onItemClickListener) {
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    onItemClickListener.onItemClick(user);
+                }
+            });
+
+            view.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View view) {
+                    onItemClickListener.onItemLongClick(user);
+                    return false;
+                }
+            });
+        }
+
+        void displayUserInfo(User user) {
+            String status;
+            int textColor;
+            if (user.getOnline()) {
+                status = "online";
+                textColor = Color.GREEN;
+            } else {
+                status = "offline";
+                textColor = Color.RED;
+            }
+            txtStatus.setText(status);
+            txtStatus.setTextColor(textColor);
+            txtUser.setText(user.getEmail());
+        }
+    }
 }
