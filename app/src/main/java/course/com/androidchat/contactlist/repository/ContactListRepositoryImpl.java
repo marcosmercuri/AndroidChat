e1683194d0c2f48d01007fb413f373f8c93ff352
Marcos Mercuri <marcosmercuri@gmail.com>
2016-11-09 23:36:39 +0100

    Feature: add the new event listener implementation

--- app/src/main/java/course/com/androidchat/contactlist/repository/ContactListRepositoryImpl.java
+++ app/src/main/java/course/com/androidchat/contactlist/repository/ContactListRepositoryImpl.java
@@ -1,8 +1,6 @@
 package course.com.androidchat.contactlist.repository;
 
 import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 
 import course.com.androidchat.helper.FirebaseHelper;
 
@@ -10,8 +8,8 @@ public class ContactListRepositoryImpl implements ContactListRepository {
     private FirebaseHelper firebaseHelper;
     private ChildEventListener contactEventListener;
 
-    public ContactListRepositoryImpl(ChildEventListener contactEventListener) {
-        this.contactEventListener = contactEventListener;
+    public ContactListRepositoryImpl() {
+        this.contactEventListener = new ContactListEventListener();
         this.firebaseHelper = FirebaseHelper.getInstance();
     }
 
@@ -35,28 +33,7 @@ public class ContactListRepositoryImpl implements ContactListRepository {
     @Override
     public void subscribeToContactListEvent() {
         if (contactEventListener==null) {
-            contactEventListener = new ChildEventListener() {
-                @Override
-                public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-
-                }
-
-                @Override
-                public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-                }
-
-                @Override
-                public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-                }
-
-                @Override
-                public void onChildMoved(DataSnapshot dataSnapshot, String s) { }
-
-                @Override
-                public void onCancelled(DatabaseError databaseError) { }
-            };
+            contactEventListener = new ContactListEventListener();
         }
         firebaseHelper.getAuthUserContactsReference().addChildEventListener(contactEventListener);
     }
