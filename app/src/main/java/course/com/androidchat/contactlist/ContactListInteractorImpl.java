7bf78d90267a9d358c6c75207812c4f671f27b29
Marcos Mercuri <marcosmercuri@gmail.com>
2016-11-09 10:53:20 +0100

    Feature: Add dummy implementations

---
+++ app/src/main/java/course/com/androidchat/contactlist/ContactListInteractorImpl.java
@@ -0,0 +1,23 @@
+package course.com.androidchat.contactlist;
+
+public class ContactListInteractorImpl implements ContactListInteractor {
+    @Override
+    public void subscribe() {
+
+    }
+
+    @Override
+    public void unsubscribe() {
+
+    }
+
+    @Override
+    public void removeContact(String email) {
+
+    }
+
+    @Override
+    public void destroyListener() {
+
+    }
+}
---
+++ app/src/main/java/course/com/androidchat/contactlist/ContactListSessionInteractorImpl.java
@@ -0,0 +1,18 @@
+package course.com.androidchat.contactlist;
+
+public class ContactListSessionInteractorImpl implements ContactListSessionInteractor {
+    @Override
+    public void signOff() {
+
+    }
+
+    @Override
+    public String getCurrentUserEmail() {
+        return null;
+    }
+
+    @Override
+    public void changeConnectionStatus(Boolean online) {
+
+    }
+}
