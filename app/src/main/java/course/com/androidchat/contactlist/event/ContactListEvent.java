4970b86ca52b1538d6f675dc01d6c56124c60103
Marcos Mercuri <marcosmercuri@gmail.com>
2016-11-09 11:13:42 +0100

    Feature: Add event for contactList

--- app/src/main/java/course/com/androidchat/contactlist/event/ContactListEvent.java
+++ app/src/main/java/course/com/androidchat/contactlist/event/ContactListEvent.java
@@ -1,4 +1,28 @@
 package course.com.androidchat.contactlist.event;
 
+import course.com.androidchat.entities.User;
+
 public class ContactListEvent {
+    public static final int ON_CONTACT_ADDED = 0;
+    public static final int ON_CONTACT_CHANGED = 1;
+    public static final int ON_CONTACT_REMOVED = 2;
+
+    private User user;
+    private int eventType;
+
+    public int getEventType() {
+        return eventType;
+    }
+
+    public void setEventType(int eventType) {
+        this.eventType = eventType;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
