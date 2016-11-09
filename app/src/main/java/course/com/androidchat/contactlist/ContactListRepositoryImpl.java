5dfbbdeb0a77284be523fef860c580424fb4df8d
Marcos Mercuri <marcosmercuri@gmail.com>
2016-11-09 11:25:31 +0100

    WIP: Add dummy ContactListRepository implementation

---
+++ app/src/main/java/course/com/androidchat/contactlist/ContactListRepositoryImpl.java
@@ -0,0 +1,38 @@
+package course.com.androidchat.contactlist;
+
+public class ContactListRepositoryImpl implements ContactListRepository {
+    @Override
+    public void signOff() {
+
+    }
+
+    @Override
+    public String getCurrentUserEmail() {
+        return null;
+    }
+
+    @Override
+    public void removeContact(String email) {
+
+    }
+
+    @Override
+    public void subscribeToContactListEvent() {
+
+    }
+
+    @Override
+    public void unsubscribeToContactListEvent() {
+
+    }
+
+    @Override
+    public void destroyListener() {
+
+    }
+
+    @Override
+    public void changeConnectionStatus(Boolean online) {
+
+    }
+}
