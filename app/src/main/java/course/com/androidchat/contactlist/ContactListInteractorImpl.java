0c8c7e62deb1930e1437d5f2bcdc368a95b908bb
Marcos Mercuri <marcosmercuri@gmail.com>
2016-11-09 11:25:48 +0100

    Feature: Implement Interactors

--- app/src/main/java/course/com/androidchat/contactlist/ContactListInteractorImpl.java
+++ app/src/main/java/course/com/androidchat/contactlist/ContactListInteractorImpl.java
@@ -1,23 +1,29 @@
 package course.com.androidchat.contactlist;
 
 public class ContactListInteractorImpl implements ContactListInteractor {
+    private ContactListRepository repository;
+
+    public ContactListInteractorImpl() {
+        repository = new ContactListRepositoryImpl();
+    }
+
     @Override
     public void subscribe() {
-
+        repository.subscribeToContactListEvent();
     }
 
     @Override
     public void unsubscribe() {
-
+        repository.unsubscribeToContactListEvent();
     }
 
     @Override
     public void removeContact(String email) {
-
+        repository.removeContact(email);
     }
 
     @Override
     public void destroyListener() {
-
+        repository.destroyListener();
     }
 }
--- app/src/main/java/course/com/androidchat/contactlist/ContactListSessionInteractorImpl.java
+++ app/src/main/java/course/com/androidchat/contactlist/ContactListSessionInteractorImpl.java
@@ -1,18 +1,24 @@
 package course.com.androidchat.contactlist;
 
 public class ContactListSessionInteractorImpl implements ContactListSessionInteractor {
+    private ContactListRepository repository;
+
+    public ContactListSessionInteractorImpl() {
+        repository = new ContactListRepositoryImpl();
+    }
+
     @Override
     public void signOff() {
-
+        repository.signOff();
     }
 
     @Override
     public String getCurrentUserEmail() {
-        return null;
+        return repository.getCurrentUserEmail();
     }
 
     @Override
     public void changeConnectionStatus(Boolean online) {
-
+        repository.changeConnectionStatus(online);
     }
 }
