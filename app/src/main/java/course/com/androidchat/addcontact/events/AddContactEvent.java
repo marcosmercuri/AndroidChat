40be86b1601c8b37d3e429b68908609a0abcfb44
Marcos Mercuri <marcosmercuri@gmail.com>
2016-11-13 12:29:32 +0100

    Feature: Implement event pojo

--- app/src/main/java/course/com/androidchat/addcontact/events/AddContactEvent.java
+++ app/src/main/java/course/com/androidchat/addcontact/events/AddContactEvent.java
@@ -1,4 +1,17 @@
 package course.com.androidchat.addcontact.events;
 
 public class AddContactEvent {
+    private Boolean error;
+
+    public AddContactEvent() {
+        this.error = false;
+    }
+
+    public Boolean getError() {
+        return error;
+    }
+
+    public void setError(Boolean error) {
+        this.error = error;
+    }
 }
