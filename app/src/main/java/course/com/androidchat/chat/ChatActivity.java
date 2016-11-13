2fd815253866eb0b26bc4e4de02e5d50f50900db
Marcos Mercuri <marcosmercuri@gmail.com>
2016-11-13 13:02:34 +0100

    Feature: Add layout for the chat screen

---
+++ app/src/main/java/course/com/androidchat/chat/ChatActivity.java
@@ -0,0 +1,14 @@
+package course.com.androidchat.chat;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import course.com.androidchat.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+    }
+}
